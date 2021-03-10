c161ccaaa87391dee1a754217a3831d87c7274ba
Kory <80230003+SilverFaux27@users.noreply.github.com>
2021-03-09 21:40:33 -0800

    Update ideas

--- get-quote.py
+++ get-quote.py
@@ -9,6 +9,13 @@ def main():
   last = last = len(quotes) - 1
   rnd = random.randint(0, last)
   print(quotes[rnd])
+  # second quote
+ # print(quotes[rnd-1])
+  #Want user input to add quote to list after getting a quote
+  #with open("quotes.txt", "a") as f:
+#    f.append(new_quote)
+
+
 
 if __name__== "__main__":
     main()
